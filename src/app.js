b1866107c5d76fe1d02a26c1fe951983bf41a676
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-21 21:09:43 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+// React dependencies
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// ReactRouter dependencies and custom history
+import AppRouter, { history } from './routers/AppRouter';
+
+// react-redux dependencies
+import { Provider } from 'react-redux';
+
+// redux imports
+import configureStore from './store/configureStore';
+
+// style imports
+import 'react-dates/lib/css/_datepicker.css';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+// component imports
+import LoadingPage from './components/LoadingPage';
+import Dashboard from './components/Dashboard';
+
+// const store = configureStore();
+
+const jsx = (
+  // <Provider store={store}>
+    <Dashboard />
+  // </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+import Dashboard from '../components/Dashboard';
+import NotFound from '../components/NotFound';
+// import PrivateRoute from './PrivateRoute';
+// import PublicRoute from './PublicRoute';
+
+export const history = createHistory();
+
+const AppRouter = () => (
+  <Router history={history}>
+    <div>
+      <Switch>
+        <Route path="/" component={Dashboard} exact={true} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default AppRouter;
