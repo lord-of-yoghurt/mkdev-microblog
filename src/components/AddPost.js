be1afd25ce977e0090de1a114af3eb314a15ab30
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-23 18:08:08 -0500

    set up AddPost and PostForm components, wire up submission

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddPost = () => (
-  <div>
-    New Post Form here...
-  </div>
-);
+import PostForm from './PostForm';
 
-export default AddPost;
+export default class AddPost extends Component {
+  onSubmit = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>New Post</h1>
+        <PostForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+};
