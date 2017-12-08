20de923691b3cb153835a2e5215943867de7dff8
David Ordovskiy <david.ordovsky@gmail.com>
2017-12-07 22:26:52 -0500

    add more tests for PostForm

--- src/tests/components/PostForm.test.js
+++ src/tests/components/PostForm.test.js
@@ -16,4 +16,45 @@ describe('PostForm component', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('shows an error message if any of the fields are empty', () => {
+    const wrapper = shallow(<PostForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('form').simulate('submit', {
+      preventDefault: () => {}
+    });
+
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('sets title on input change', () => {
+    const value = 'blabbety blah';
+    const wrapper = shallow(<PostForm />);
+    wrapper.find('input').at(0).simulate('change', {
+      target: { value }
+    });
+
+    expect(wrapper.state('title')).toBe(value);
+  });
+
+  it('sets body on input change', () => {
+    const value = 'i wrote a cool post check it out';
+    const wrapper = shallow(<PostForm />);
+    wrapper.find('textarea').simulate('change', {
+      target: { value }
+    });
+
+    expect(wrapper.state('body')).toBe(value);
+  });
+
+  it('sets username on input change', () => {
+    const value = 'admin';
+    const wrapper = shallow(<PostForm />);
+    wrapper.find('input').at(1).simulate('change', {
+      target: { value }
+    });
+
+    expect(wrapper.state('username')).toBe(value);
+  });
 });
